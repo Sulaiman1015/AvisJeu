458b6388d68d7b6e4d24691abe78a3c234b43ce3
Thibaut <tibeloute@msn.com>
2022-07-07 12:13:59 +0200

    insertion des class business

--- src/main/java/fr/orsys/su/AvisJeu/bussiness/ModeleEconomique.java
+++ src/main/java/fr/orsys/su/AvisJeu/bussiness/ModeleEconomique.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,14 +19,14 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 
-public abstract class Utilisateur {
+public class ModeleEconomique {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    protected Long id;
-    protected String pseudo;
-    protected String motDePasse;
-    protected String email;
+    private Long id;
+    private String nom;
 
+    @ManyToOne
+    private Jeu jeu;
 }
 
